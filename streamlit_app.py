1f2a5306e4e3b9c424f686bf9ffe2e4fc9aa3f3f
viraltarpara85 <114025408+viraltarpara85@users.noreply.github.com>
2022-09-30 15:31:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,8 +34,8 @@ try:
   else:
     back_from_function= get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
- except URLError as e:
-    streamlit.error()
+except URLError as e:
+  streamlit.error()
   
   
 streamlit.stop()
