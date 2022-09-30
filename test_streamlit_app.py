7eb7367299d012839cd07e35074a3a7d415e05cb
viraltarpara85 <114025408+viraltarpara85@users.noreply.github.com>
2022-09-30 12:38:36 -0500

    Create test_streamlit_app.py

---
+++ test_streamlit_app.py
@@ -0,0 +1,31 @@
+import streamlit
+import pandas
+import requests
+import snowflake.connector
+
+
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text(' [🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('[🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('[🍀Hard-Boiled Free-Range Egg')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('Fruityvice Fruit Advice!')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+my_fruit_list = my_fruit_list.set_index('Fruit')
+fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+streamlit.header("Fruityvice Fruit Advice!")
+
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
