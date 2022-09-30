dff579ec03789ab29df4c5bebca34466d8591d4f
viraltarpara85 <114025408+viraltarpara85@users.noreply.github.com>
2022-09-29 21:49:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ streamlit.text(' [🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('[🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('[🍀Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
+streamlit.header('Fruityvice Fruit Advice!')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
