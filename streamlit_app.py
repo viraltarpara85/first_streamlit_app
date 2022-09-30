d4b0e9d6f3d09f8e31c993f363c590f9772dd887
viraltarpara85 <114025408+viraltarpara85@users.noreply.github.com>
2022-09-30 14:52:41 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,12 +20,6 @@ fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.in
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
-streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-
-
 
 streamlit.header("Fruityvice Fruit Advice!")
 try:
