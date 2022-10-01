00b6bd4c970dcfcf8b4ea2434c81d65272d1ac20
viraltarpara85 <114025408+viraltarpara85@users.noreply.github.com>
2022-10-01 11:47:19 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,10 +38,10 @@ except URLError as e:
   streamlit.error()
   
   
-streamlit.stop()
+#streamlit.stop()
 
 # write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 
 streamlit.header("The fruit load list contains:")
