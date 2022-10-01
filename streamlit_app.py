c6cc9e17c1d34db7ebca3eeb1d10a807ac22da9a
viraltarpara85 <114025408+viraltarpara85@users.noreply.github.com>
2022-10-01 10:53:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ streamlit.dataframe(fruits_to_show)
 
 def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-  fruityvice_normalized = pandas.json_normlize(fruityvice_response.json())
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
 
 # New section to display Fruityvice api response
